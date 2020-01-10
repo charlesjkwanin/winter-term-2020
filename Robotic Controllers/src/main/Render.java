f2e0d26ae9d5a815c074263e7d85c0f80cb91d41
Charles <lycoinjunior@gmail.com>
2020-01-10 12:14:50 -0500

    Initial commit

---
+++ Robotic Controllers/src/main/Render.java
@@ -0,0 +1,28 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Render extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("main.Render");
+    }
+
+    @Override
+    public void settings() {
+        // TODO: Customize screen size and so on here
+        size(400, 400);
+
+    }
+
+    @Override
+    public void setup() {
+        // TODO: Your custom drawing and setup on applet start belongs here
+
+    }
+
+    @Override
+    public void draw() {
+        // TODO: Do your drawing for each frame here
+
+    }
+}

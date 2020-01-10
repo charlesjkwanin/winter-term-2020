44bf3a3a3f31241fa037b945db0235fcd49154e6
Charles <lycoinjunior@gmail.com>
2020-01-10 07:50:56 -0500

    changed access modifiers to package-private

--- Traveling Salesman/src/main/TourManager.java
+++ Traveling Salesman/src/main/TourManager.java
@@ -2,22 +2,22 @@ package main;
 
 import java.util.ArrayList;
 
-public class TourManager {
+class TourManager {
     private static ArrayList<City> destinationCities = new ArrayList<>();
 
-    public static void addCity(City city) {
+    static void addCity(City city) {
         destinationCities.add(city);
     }
 
-    public static City getCity(int index) {
+    static City getCity(int index) {
         return destinationCities.get(index);
     }
 
-    public static int numberOfCities() {
+    static int numberOfCities() {
         return destinationCities.size();
     }
 
-    public static ArrayList<City> getDestinationCities() {
+    static ArrayList<City> getDestinationCities() {
         return destinationCities;
     }
 
